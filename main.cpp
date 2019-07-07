56abe013e0a991daefddaeee68351a11091937bc
ReznoRMichael <mic3arena@gmail.com>
2019-07-07 12:05:12 +0200

    cleaned code a bit, added some comments

--- main.cpp
+++ main.cpp
@@ -2,44 +2,60 @@
 
 using namespace std;
 
+// abstract class Ksztalt
+
 class Ksztalt
 {
 public:
-    virtual void oblicz_pole() = 0;     // czysta funkcja wirtualna
+
+    // clean virtual function
+    virtual void oblicz_pole() = 0;
 };
-///////////////////////////////////
+
+/* ------------- class Kolo is inheriting from the abstract class Ksztalt ---------------- */
 
 class Kolo :public Ksztalt
 {
     float r;
+
 public:
+
     Kolo(float x)
     {
         r = x;
     }
+
+    // virtual member function does different things for the Kolo class than for the Kwadrat class
     virtual void oblicz_pole()
     {
         cout << "Pole kola: " << 3.14*r*r << endl;
     }
 };
 
+/* ------------- class Kwadrat is inheriting from the abstract class Ksztalt ---------------- */
+
 class Kwadrat :public Ksztalt
 {
     float a;
+
 public:
+
     Kwadrat(float x)
     {
         a = x;
     }
+
+    // virtual member function does different things for the Kwadrat class than for the Kolo class
     virtual void oblicz_pole()
     {
         cout << "Pole kwadratu: " << a*a << endl;
     }
 };
 
-//////////////////////////////
+/* --------------------------------------------- */
 
-void obliczenia(Ksztalt *wsk)   // uniwersalna funkcja
+// universal function for calling the virtual member functions on the class objects
+void obliczenia(Ksztalt *wsk)
 {
     wsk -> oblicz_pole();
 }
@@ -57,9 +73,11 @@ int main()
     wskaznik = &kw;
     wskaznik -> oblicz_pole();
 
-    obliczenia(wskaznik);   // z ostatniej zmiennej w pamieci (kw)
+    // call the function on the last variable in the memory (kw)
+    obliczenia(wskaznik);
 
-    obliczenia(&k);     // z obiektu k
+    // call the function on the object k (reference / address)
+    obliczenia(&k);
 
     return 0;
 }
